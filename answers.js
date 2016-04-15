1b5e360f40a1cd1c7528e4710f5fdb7e6c31a794
jbain1 <jbainschool@gmail.com>
2016-04-15 17:41:43 +0000

    Finished exercises

---
+++ answers.js
@@ -0,0 +1,61 @@
+/* Exercise 1
+
+Write a function called printPositives that takes an array and uses its forEach method to print only the positive numbers.*/
+
+function printPositives(ary) {
+    ary.forEach(function(each){
+        if (each > 0) {
+            console.log(each)
+        };
+    })
+}
+
+//printPositives([1,2,3,-4])
+
+/* Exercise 2
+
+Similar to the previous exercise, write a function called getPositives that takes an array and uses its filter method to return a 
+new array with only the positive numbers. */
+
+function getPositives (array) {
+    var newary = [];
+    array.filter(function(each){
+        if (each > 0) {
+            newary.push(each);
+        }
+       
+    })
+    return newary
+}
+
+//console.log(getPositives([1,2,3,-4]))
+
+/* Exercise 3
+
+Write a function called filterArray that takes an array AND a function as arguments. Your filter function should return a new array 
+that contains only the elements where the passed function returns a truthy value.
+
+NOTE: You are allowed to use Array.prototype.filter to answer this question. */
+
+function isitPositive (num) {
+        if (num > 0) {
+            return true;
+        }
+       else {
+           return false;
+       }
+}
+
+
+function filterArray (array, callback) {
+    var result = [];
+    for(var i = 0; i < array.length; i++) {
+        if(callback(array[i])) {  
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+
+console.log(filterArray([1,2,3,-4], isitPositive))
+
